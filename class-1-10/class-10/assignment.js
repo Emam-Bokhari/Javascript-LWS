8ca7a33cf9dd5d4fe73b43e9e27ab0d241b236fc
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-05 20:55:04 +0600

    assignment operators

---
+++ class-1-10/class-10/assignment.js
@@ -0,0 +1,33 @@
+// addition
+let x=10
+let y=15
+x+=10
+console.log(x+y)
+// substraction
+let a=10
+a-=5
+console.log(a)
+// multiplication
+let b=10
+b*=2
+console.log(b)
+// division
+let c=10
+c/=2
+console.log(c)
+// modulus
+let d=10
+d%=3
+console.log(d)
+// exponentiation
+let e=2
+e**=3
+console.log(e)
+// increament
+let f=5
+f++
+console.log(f)
+// decrement
+let g=5
+g--
+console.log(g)
--- class-1-10/class-7/let.js
+++ class-1-10/class-7/let.js
@@ -7,3 +7,6 @@ console.log(x) */
     let x = 10;
     console.log(x)
 }
+
+const x=20
+console.log(typeof(x))
