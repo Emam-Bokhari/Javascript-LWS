c5388a177bc2ac48f114514ed728097e656a5335
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-15 20:47:23 +0600

    var,let,const hoisting

---
+++ Revise/Class-08.js
@@ -0,0 +1,15 @@
+// Class:08
+// var hoisting
+carName="Volvo";
+var carName;
+console.log(carName);
+
+// let hoisting
+carNameLet="BMW";
+let carNameLet;
+console.log(carNameLet);
+
+// const hoisting
+const carNameConst;
+carNameConst="Mercedece";
+console.log(carNameConst);
