ec496482736b1e7bc1873cbcd011cdac213f02e3
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-15 21:07:25 +0600

    data type

---
+++ Revise/Class-11.js
@@ -0,0 +1,19 @@
+// Class:11
+const a = 20 + "Volvo";
+
+const b = "Volvo" + 20;
+
+const c = 2 + 3 + "Volvo";
+
+const d = "Volvo" + 3 + 3;
+
+const e = 3 + 3 + 1 + "Volvo";
+
+const f = "Volvo" + 3 + 5 + 2;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
