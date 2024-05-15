9debc94fc11348dea1bfd20ea11c329d90af11c3
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-15 22:35:21 +0600

    string methods

---
+++ Revise/Class-16.js
@@ -0,0 +1,19 @@
+const x=[1,2,3,4,5];
+console.log(x.slice(0,2));
+
+
+const y="Emam Bokhari";
+const z="Moshfiqur Rahman";
+console.log(y.slice(0,5));
+
+console.log(y.split(" "));
+
+console.log(y.trim());
+
+console.log(y.charAt(5));
+
+console.log(y.toUpperCase());
+
+console.log(y.toLowerCase());
+
+console.log("replace",y.replace())
