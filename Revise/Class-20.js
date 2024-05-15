2e745b0f9758f30b8f024b0b50bcd49964793c3e
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-15 23:00:27 +0600

    Number

---
+++ Revise/Class-20.js
@@ -0,0 +1,13 @@
+// Class-20
+const x=20.005;
+const y="20";
+console.log(x.toString());
+
+console.log(x.toFixed(2));
+
+
+console.log(parseFloat(x));
+
+console.log(parseInt(x))
+
+console.log(Number(y))
