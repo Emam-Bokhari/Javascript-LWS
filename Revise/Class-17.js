024b7e04e918fb8ecada5d0b086c9dcdb9258df6
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-15 22:42:34 +0600

    string search

---
+++ Revise/Class-17.js
@@ -0,0 +1,13 @@
+// Class-17
+const x = "Emam Bokhari";
+console.log(x.indexOf("B"));
+
+console.log(x.lastIndexOf("a"));
+
+console.log(x.startsWith("Emam"));
+
+console.log(x.endsWith("Bokhari"));
+
+console.log(x.includes("E"))
+
+console.log(x.toLowerCase().includes("e"))
