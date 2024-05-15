678d1b94806a985d70fc39c908d54b84378151e1
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-15 20:54:47 +0600

    typeOf

---
+++ Revise/Class-09.js
@@ -0,0 +1,15 @@
+// Class-09
+const a = 20;
+const b = "Hello World!";
+const c = [1, 2, 3];
+const d = { x: 10, y: 11 };
+function e() {
+    console.log("Function");
+};
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+
