d1563f2a807f87180396f5f85e139fd4e75ab96f
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-29 22:57:42 +0600

    for in loop, for of loop

---
+++ Class-31-40/Class-36/for-in-loop.js
@@ -0,0 +1,16 @@
+// Class:36
+const person = {
+    name: "Emam Bokhari",
+    age: 24,
+    address: "Mojlishpur,Kasba,Brahmanbaria"
+}
+
+for (let x in person) {
+    console.log(person[x])
+}
+
+const number = [1, 2, 3, 4, 5, 6, 7];
+
+for(let x in number){
+    console.log(number[x])
+}
---
+++ Class-31-40/Class-37/for-of-loop.js
@@ -0,0 +1,16 @@
+// Class:37
+const person = {
+    name: "Emam Bokhari",
+    age: 24
+};
+
+const numbers = [21,22,27];
+
+for (let x of numbers) {
+    console.log(x)
+};
+
+
+for(let a in person){
+    console.log(a)
+}
