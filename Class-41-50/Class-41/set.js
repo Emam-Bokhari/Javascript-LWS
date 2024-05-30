393a0b4bfb95e225ff0040df18d75723b787ba93
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-30 21:05:40 +0600

    set

---
+++ Class-41-50/Class-41/set.js
@@ -0,0 +1,23 @@
+// Class : 41
+// set
+
+const mySet = new Set(["a", "b", "c"]);
+
+console.log(mySet);
+
+// add
+mySet.add("d");
+
+console.log(mySet);
+
+// length
+console.log(mySet.size);
+
+const x=new Set();
+
+x.add("a");
+
+const y="c"
+x.add(y)
+
+console.log(x);
