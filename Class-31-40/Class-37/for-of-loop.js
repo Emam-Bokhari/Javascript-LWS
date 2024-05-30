b7521d0db1bdf09a4ef16c934c99e3befa32f760
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-30 20:39:00 +0600

    while,do while loop,infinite loop

--- Class-31-40/Class-37/for-of-loop.js
+++ Class-31-40/Class-37/for-of-loop.js
@@ -4,13 +4,27 @@ const person = {
     age: 24
 };
 
-const numbers = [21,22,27];
+const numbers = [21, 22, 27];
 
 for (let x of numbers) {
     console.log(x)
 };
 
 
-for(let a in person){
+for (let a in person) {
     console.log(a)
 }
+
+
+// const a = 0.1
+// const b = 0.2
+// const c = 0.3
+// const d = 0.4
+// const e = 0.5
+// const f = 0.6
+// const g = 0.7
+
+// console.log((f*10+g*10)/10,"ans")
+
+// console.log(d+e)
+// console.log(f+g)
---
+++ Class-31-40/Class-38/while-loop.js
@@ -0,0 +1,24 @@
+// Class:38
+// while loop
+
+let i = 0;
+
+while (i <= 10) {
+    console.log(i);
+    i++
+};
+
+// do while loop
+let j=0
+do {
+    console.log("Emam");
+    j++
+} while (j <= 5);
+
+
+// infinite loop
+// while(true){
+//     console.log(i)
+//     i++
+// }
+
