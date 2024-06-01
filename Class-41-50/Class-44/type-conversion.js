cb6d961f9039b2fa54814d57c1f25574f6ca8454
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-06-01 20:12:27 +0600

    converting string to number

---
+++ Class-41-50/Class-44/type-conversion.js
@@ -0,0 +1,19 @@
+// Class:44
+// type conversion
+
+// converting string to number
+console.log(Number("Emam Bokhari"));
+
+console.log(Number(""));
+
+console.log(Number("    "));
+
+console.log(Number("2"));
+
+console.log(Number("2.3"));
+
+console.log(parseInt("3"));
+
+console.log(parseFloat("3.5"))
+
+console.log(+"5");
